028b0e70a12756984c26e5a5a867c9467c557688
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-06-09 21:52:43 +0200

    create: exersiseTwo

--- js/loopTraining/loop.js
+++ js/loopTraining/loop.js
@@ -23,6 +23,7 @@
 
 const resultDiv = document.querySelector(".output");
 
+function exersiseOne(){
 for(i=10; i>=0; i--){
     console.log(i);
     const para = document.createElement('p');
@@ -36,9 +37,46 @@ for(i=10; i>=0; i--){
     resultDiv.appendChild(para);
 
 }
+}
 
 // ###############################
  // Übung 2
  // ##############################
 
- // tbd
+//  Active learning: Filling in a guest list
+//  In this exercise, we want you to take a list of names stored in an array and put them into a guest list. But it's not quite that easy — we don't want to let Phil and Lola in because they are greedy and rude, and always eat all the food! We have two lists, one for guests to admit, and one for guests to refuse.
+ 
+//  Specifically, we want you to:
+ 
+//  Write a loop that will iterate through the people array.
+//  During each loop iteration, check if the current array item is equal to "Phil" or "Lola" using a conditional statement:
+//  If it is, concatenate the array item to the end of the refused paragraph's textContent, followed by a comma and a space.
+//  If it isn't, concatenate the array item to the end of the admitted paragraph's textContent, followed by a comma and a space.
+//  We've already provided you with:
+ 
+//  refused.textContent += — the beginnings of a line that will concatenate something at the end of refused.textContent.
+//  admitted.textContent += — the beginnings of a line that will concatenate something at the end of admitted.textContent.
+//  Extra bonus question — after completing the above tasks successfully, you will be left with two lists of names, separated by commas, but they will be untidy — there will be a comma at the end of each one. Can you work out how to write lines that slice the last comma off in each case, and add a full stop to the end? Have a look at the Useful string methods article for help.
+ 
+//  If you make a mistake, you can always reset the example with the "Reset" button. If you get really stuck, press "Show solution" to see a solution.
+
+const resultDiv2 = document.querySelector(".output2"); 
+
+function exersiseTwo(){
+    const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+    const admitted = document.querySelector('.admitted');
+    const refused = document.querySelector('.refused');
+
+    admitted.textContent = 'Admit: ';
+    refused.textContent = 'Refuse: ';
+
+    for(const person of people){
+        if(person === "Phil" || person === "Lola"){
+            refused.textContent += person + ", ";
+        }else{
+            admitted.textContent += person + ", ";
+        }
+    }
+
+    // tbd. Komma am Ende der Strings entferen
+ }
