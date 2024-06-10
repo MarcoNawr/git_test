7851f3f6dd80ca5e0fc0a40d6c504ef524c2cd31
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-06-10 06:54:30 +0200

    update: description of exersise

--- js/loopTraining/loop.js
+++ js/loopTraining/loop.js
@@ -93,6 +93,11 @@ function exersiseTwo(){
  // Übung 3
  // ##############################
 
+//  https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Test_your_skills:_Loops
+//  In our first looping task we want you to start by creating a simple loop that goes through all the items
+//   in the provided myArray and prints them out on the screen inside list items (i.e., <li> elements), 
+//     which are appended to the provided list.
+
  function exerciseThree(){
     const myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
     const list = document.createElement('ul');
