d31004e091ca7c68915e67400f98fa7cc00d9336
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-06-10 20:48:50 +0200

    create: exercise four and five

--- js/loopTraining/loop.js
+++ js/loopTraining/loop.js
@@ -111,3 +111,88 @@ function exersiseTwo(){
     const section = document.querySelector('section');
     section.appendChild(list);
  }
+
+ // ###############################
+ // Übung 4
+ // ##############################
+
+//  https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Test_your_skills:_Loops
+// In this next task, we want you to write a simple program that, given a name, searches an array of objects containing names and phone numbers (phonebook) and, if it finds the name, outputs the name and phone number into the paragraph (para) and then exits the loop before it has run its course.
+
+// If you haven't read about objects yet, don't worry! For now, all you need to know is how to access a member-value pair. You can read up on objects in the JavaScript object basics tutorial.
+
+// You are given three variables to begin with:
+
+// name — contains a name to search for
+// para — contains a reference to a paragraph, which will be used to report the results
+// phonebook - contains the phonebook entries to search.
+// You should use a type of loop that you've not used in the previous task.
+
+function exerciseFour(){
+    const name = 'Mustafa';
+    const para = document.createElement('p');
+
+    const phonebook = [
+      { name : 'Chris', number : '1549' },
+      { name : 'Li Kang', number : '9634' },
+      { name : 'Anne', number : '9065' },
+      { name : 'Francesca', number : '3001' },
+      { name : 'Mustafa', number : '6888' },
+      { name : 'Tina', number : '4312' },
+      { name : 'Bert', number : '7780' },
+      { name : 'Jada', number : '2282' },
+    ]
+
+    // Add your code here
+    for(const person of phonebook){
+        if(person.name === "Mustafa"){
+            para.textContent = "Name: " + person.name + " Phone: " + person.number
+            break;
+        }else{}
+    }
+
+    // Don't edit the code below here!
+    const section = document.querySelector('.section2');
+    section.appendChild(para);
+}
+
+ // ###############################
+ // Übung 5
+ // ##############################
+
+//  https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Test_your_skills:_Loops
+// In this final task, you are provided with the following:
+
+// i — starts off with a value of 500; intended to be used as an iterator.
+// para — contains a reference to a paragraph, which will be used to report the results.
+// isPrime() — a function that, when passed a number, returns true if the number is a prime number, and false if not.
+// You need to use a loop to go through the numbers 2 to 500 backwards (1 is not counted as a prime number), and run the provided isPrime() function on them. 
+// For each number that isn't a prime number, continue on to the next loop iteration. For each one that is a prime number, add it to the paragraph's textContent along with some kind of separator.
+
+// You should use a type of loop that you've not used in the previous two tasks.
+
+function exerciseFive(){
+    let i = 500;
+    const para = document.createElement('p');
+
+    function isPrime(num) {
+      for(let i = 2; i < num; i++) {
+        if(num % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+
+    // Add your code here
+    for(j=i; j>=2; j--){
+        if(isPrime(j)){
+            para.innerHTML += j + " ist eine Primezahl <br>";
+        }else{}
+    }
+
+    // Don't edit the code below here!
+    const section = document.querySelector('.section3');
+    section.appendChild(para);
+}
