48e5b504f6c000051938d7aedf2a3d09462acff5
Marco <166357601+MarcoNawr@users.noreply.github.com>
2024-07-01 14:12:25 +0200

    refactor: bether way t owrite method within object

--- object_basics/object_basics.js
+++ object_basics/object_basics.js
@@ -1,10 +1,14 @@
 const person = {
-    name: ["Bob", "Smith"],
+    name: {
+        first: "Bob",
+        last: "Smith",
+    },
     age: 32,
-    bio: function (){
-        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old`);
+    bio() {
+      console.log(`${this.name.first} ${this.name.last} is ${this.age} years old.`);
     },
-    introduceSelf: function(){
-        console.log(`Hi Iam ${this.name[0]}.`);
+    introduceSelf() {
+      console.log(`Hi! I'm ${this.name.first}.`);
     },
-};
+  };
+  
